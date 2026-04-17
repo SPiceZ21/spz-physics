14b935b5fb266638f23acec963003e9ed7d186e6
Yuga Bharathi Jaisankar <yuga.bharathijai2106@gmail.com>
2026-04-17 12:11:08 +0530

    feat(physics): implement anti-roll bar physics

--- client/swaybar.lua
+++ client/swaybar.lua
@@ -1,13 +1,17 @@
 -- client/swaybar.lua
+
 local function ApplySwaybars(vehicle, profile)
+  if not profile.swaybar then return end
+  
   local sb = profile.swaybar
   local totalStiffness = (sb.front_strength + sb.rear_strength) / 100.0
-  local frontShare = sb.front_bias
+  local frontShare = sb.front_bias or 0.5
 
-  SetVehicleHandlingFloat(vehicle, "CHandlingData",
-    "fAntiRollBarForce",       totalStiffness * 0.8)
-  SetVehicleHandlingFloat(vehicle, "CHandlingData",
-    "fAntiRollBarBiasFront",   frontShare)
+  -- fAntiRollBarForce: 0.0 to 1.0ish
+  SetVehicleHandlingFloat(vehicle, "CHandlingData", "fAntiRollBarForce", totalStiffness * 0.8)
+  
+  -- fAntiRollBarBiasFront: 0.0 to 1.0 (0.5 is equal)
+  SetVehicleHandlingFloat(vehicle, "CHandlingData", "fAntiRollBarBiasFront", frontShare)
 end
 
 SPZSwaybar = {
