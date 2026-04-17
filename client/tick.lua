593dd269808c5587e354f3770b9835d1c477932f
Yuga Bharathi Jaisankar <yuga.bharathijai2106@gmail.com>
2026-04-17 12:11:34 +0530

    feat(physics): implement main physics tick loop

---
+++ client/tick.lua
@@ -0,0 +1,50 @@
+-- client/tick.lua
+
+CreateThread(function()
+    while true do
+        if PhysicsState and PhysicsState.loaded then
+            local vehicle = PhysicsState.vehicle
+            local profile = PhysicsState.profile
+            local throttle = GetControlValue(0, 71) / 127.0 -- 0.0 to 1.0 (accelerate)
+            local brake = GetControlValue(0, 72) / 127.0 -- 0.0 to 1.0 (brake)
+            local speed = GetEntitySpeed(vehicle)
+            
+            -- 1. Engine & Gearbox
+            local currentRpmNormalized = GetVehicleCurrentRpm(vehicle)
+            local realRpmTarget = profile.engine.rpm_min + (currentRpmNormalized * (profile.engine.rpm_max - profile.engine.rpm_min))
+            
+            -- Apply flywheel inertia
+            PhysicsState.rpm = SPZFlywheel.UpdateRpm(realRpmTarget, profile.flywheel and profile.flywheel.inertia)
+            
+            -- Transmission
+            if profile.gearbox.type == "Auto" or profile.gearbox.type == "Sequential" then
+                SPZGearbox.AutoShift(vehicle, profile, PhysicsState.rpm)
+            end
+            PhysicsState.gear = GetVehicleCurrentGear(vehicle)
+            
+            -- 2. Turbo Simulation
+            PhysicsState.boost_bar = SPZTurbo.UpdateBoost(vehicle, profile, PhysicsState.rpm, throttle)
+            
+            -- 3. Assists Intervention
+            -- Torquemultiplier is reset by UpdateBoost, but Assists may cut it further
+            PhysicsState.tcs_active = SPZAssists.UpdateTCS(vehicle, profile, throttle)
+            PhysicsState.abs_active = SPZAssists.UpdateABS(vehicle, profile, brake)
+            PhysicsState.lc_active  = SPZAssists.UpdateLC(vehicle, profile, speed, throttle, brake)
+            
+            -- 4. Tyre Model
+            SPZTyre.ApplyTyrePhysics(vehicle, profile, speed)
+            
+            -- 5. Final torque modulation (Power Curve)
+            local powerFactor = SPZEngine.GetPowerAtRPM(PhysicsState.rpm, profile.engine.power_curve)
+            local currentTorqueMult = GetVehicleEngineTorqueMultiplier(vehicle)
+            SetVehicleEngineTorqueMultiplier(vehicle, currentTorqueMult * powerFactor)
+            
+            -- 6. HUD Sync (Statebag)
+            SyncPhysicsStateToBag(PhysicsState)
+            
+            Wait(Config.TickRate or 0)
+        else
+            Wait(500)
+        end
+    end
+end)
